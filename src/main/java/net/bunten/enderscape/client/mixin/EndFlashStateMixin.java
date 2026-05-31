2900d0f6ecc4ef7833c100c945bae78cc9777f42
tsu <99662659+Tsuzuito@users.noreply.github.com>
2026-05-31 16:43:54 +0300

    Attempt to fix EndFlashState clock

--- src/main/java/net/bunten/enderscape/client/mixin/EndFlashStateMixin.java
+++ src/main/java/net/bunten/enderscape/client/mixin/EndFlashStateMixin.java
@@ -1,21 +1,67 @@
 package net.bunten.enderscape.client.mixin;
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import net.bunten.enderscape.client.world.EndFlashParameters;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.EndFlashState;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(EndFlashState.class)
 public abstract class EndFlashStateMixin {
 
+    @Shadow
+    private int offset;
+
+    @Shadow
+    private int duration;
+
     @Inject(method = "tick", at = @At("HEAD"), cancellable = true)
-    public void Enderscape$disableFlashTick(long l, CallbackInfo info) {
-        if (!EndFlashParameters.enabled()) info.cancel();
+    public void Enderscape$disableFlashTick(long clockTime, CallbackInfo info) {
+        if (!EndFlashParameters.enabled()) {
+            info.cancel();
+            return;
+        }
+
+        if (clockTime == 0L) {
+            Minecraft client = Minecraft.getInstance();
+            if (client.level != null) {
+                long realTime = client.level.getGameTime();
+                ((EndFlashState)(Object)this).tick(realTime);
+                info.cancel();
+            }
+        }
+    }
+
+    @ModifyExpressionValue(
+            method = "calculateFlashParameters",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/util/Mth;randomBetweenInclusive(Lnet/minecraft/util/RandomSource;II)I",
+                    ordinal = 1
+            )
+    )
+    private int changeDuration(int original) {
+        if (EndFlashParameters.updatedVisuals()) return original * 3;
+        return original;
+    }
+
+    @Inject(method = "calculateIntensity", at = @At("HEAD"), cancellable = true)
+    public void Enderscape$changeIntensityCurve(long clockTime, CallbackInfoReturnable<Float> info) {
+        if (clockTime == 0L) {
+            info.setReturnValue(0.0F);
+            return;
+        }
+        if (EndFlashParameters.updatedVisuals()) {
+            info.setReturnValue(EndFlashParameters.getIntensity(offset, duration, clockTime % EndFlashParameters.frequencyInTicks()));
+        }
     }
 
     @ModifyConstant(method = "calculateFlashParameters", constant = @Constant(longValue = 600L))
