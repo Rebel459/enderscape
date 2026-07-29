d2ac369fe773b71c2730389cc66c69d42486e352
Rebel459 <131760345+Rebel459@users.noreply.github.com>
2026-07-29 11:32:09 +1000

    fix shulker ai crash

--- src/main/java/net/bunten/enderscape/mixin/ShulkerAttackGoalMixin.java
+++ src/main/java/net/bunten/enderscape/mixin/ShulkerAttackGoalMixin.java
@@ -17,15 +17,18 @@ import java.util.List;
 @Mixin(targets = "net.minecraft.world.entity.monster.Shulker$ShulkerAttackGoal")
 public abstract class ShulkerAttackGoalMixin extends Goal {
 
+    @Shadow(aliases = "this$0")
+    @Final
+    private Shulker shulker;
+
     @Shadow
     private int attackTime;
 
-    @Inject(method = "tick", at = @At(value = "HEAD"), cancellable = true)
-    private void tick(CallbackInfo info) {
-        Shulker shulker = Shulker.class.cast(this);
+    @Inject(method = "tick", at = @At("HEAD"), cancellable = true)
+    private void tick(CallbackInfo ci) {
         if (EnderscapeConfig.getInstance().shulkerBulletEnforceCountLimit > 0 && this.attackTime % 20 == 0) {
-            List<Entity> entities = shulker.level().getEntities(shulker, shulker.getBoundingBox().inflate(50), (entity) -> entity instanceof ShulkerBullet bullet && bullet.getOwner() == shulker);
-            if (entities.size() >= EnderscapeConfig.getInstance().shulkerBulletEnforceCountLimit) info.cancel();
+            List<Entity> entities = shulker.level().getEntities(shulker, shulker.getBoundingBox().inflate(50), entity -> entity instanceof ShulkerBullet bullet && bullet.getOwner() == shulker);
+            if (entities.size() >= EnderscapeConfig.getInstance().shulkerBulletEnforceCountLimit) ci.cancel();
         }
     }
 }
